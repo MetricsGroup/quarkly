445d23a8e2a7c7d3b7d28edd6ad57706a3555e0e
Nikhil Nandagopal <nikhil.nandagopal@gmail.com>
2021-06-21 16:47:59 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ export default (() => {
 				About Us
 			</Text>
 			<Text as="p" font="--lead" margin="20px 0 0 0">
-				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.{" "}
+				Hi!it me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.{" "}
 			</Text>
 			<Box display="flex" margin="40px 0 20px 0" justify-content="space-around" sm-flex-direction="column">
 				<Box padding="10px">
