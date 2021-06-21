a7d771e637df9e305f21faf44d69d4aa60d866db
Nikhil Nandagopal <nikhil.nandagopal@gmail.com>
2021-06-21 16:48:26 +0530

    changed para

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import theme from "theme";
 import { Theme, Link, Text, Image, Box, Section } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
 import { GlobalQuarklyPageStyles } from "global-page-styles";
+import * as Components from "components";
 export default (() => {
 	return <Theme theme={theme}>
 		<GlobalQuarklyPageStyles pageUrl={"index"} />
@@ -18,7 +19,7 @@ export default (() => {
 				About Us
 			</Text>
 			<Text as="p" font="--lead" margin="20px 0 0 0">
-				Hi!it me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.{" "}
+				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.{" "}
 			</Text>
 			<Box display="flex" margin="40px 0 20px 0" justify-content="space-around" sm-flex-direction="column">
 				<Box padding="10px">
@@ -32,6 +33,8 @@ export default (() => {
 				</Box>
 			</Box>
 		</Section>
+		<Components.EmbedHTML />
+		<Components.EmbedJS />
 		<Link
 			font={"--capture"}
 			font-size={"10px"}
